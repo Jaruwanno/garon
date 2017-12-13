0ca64489dc7381dff30e1bf22876176f995fa7c6
Jaruwanno <jaruwanno1991@gmail.com>
2017-12-13 09:03:10 +0700

    ga

---
+++ local/vendor/nesbot/carbon/src/Carbon/Lang/en.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return array(
+    /*'year' => '1 year|:count years',
+    'y' => '1yr|:countyrs',
+    'month' => '1 month|:count months',
+    'm' => '1mo|:countmos',
+    'week' => '1 week|:count weeks',
+    'w' => '1w|:countw',
+    'day' => '1 day|:count days',
+    'd' => '1d|:countd',
+    'hour' => '1 hour|:count hours',
+    'h' => '1h|:counth',
+    'minute' => '1 minute|:count minutes',
+    'min' => '1m|:countm',
+    'second' => '1 second|:count seconds',
+    's' => '1s|:counts',
+    'ago' => ':time ago',
+    'from_now' => ':time from now',
+    'after' => ':time after',
+    'before' => ':time before',*/
+    'year' => '1 ปี|:count ปี',
+    'y' => '1 ปี|:count ปี',
+    'month' => '1 เดือน|:count เดือน',
+    'm' => '1 เดือน|:count เดือน',
+    'week' => '1 สัปดาห์|:count สัปดาห์',
+    'w' => '1 สัปดาห์|:count สัปดาห์',
+    'day' => '1 วัน|:count วัน',
+    'd' => '1 วัน|:count วัน',
+    'hour' => '1 ชั่วโมง|:count ชั่วโมง',
+    'h' => '1 ชั่วโมง|:count ชั่วโมง',
+    'minute' => '1 นาที|:count นาที',
+    'min' => '1 นาที|:count นาที',
+    'second' => '1 วินาที|:count วินาที',
+    's' => '1 วินาที|:count วินาที',
+    'ago' => ':time ที่แล้ว',
+    'from_now' => ':time จากนี้',
+    'after' => 'หลัง:time',
+    'before' => 'ก่อน:time',
+);
