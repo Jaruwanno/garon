b1a16438ec457eb216272f21fd4639cb70c88265
Jaruwanno <jaruwanno1991@gmail.com>
2017-12-13 22:04:19 +0700

    fd

--- local/app/Http/Controllers/Backend/HighlightController.php
+++ local/app/Http/Controllers/Backend/HighlightController.php
@@ -47,7 +47,7 @@ class HighlightController extends Controller
 
       $filesystem = new Filesystem($adapter);
 
-      dd($adapter, $filesystem, $client->listForlder());
+      dd($adapter, $filesystem, $client->listFolder());
       // $zones = Zone::orderBy('length')->get();
       //
       // $aStyle = array(
