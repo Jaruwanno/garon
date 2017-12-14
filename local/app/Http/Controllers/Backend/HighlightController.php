ae51e863307a50bf4764854c70f63518fdd7ced9
Jaruwanno <jaruwanno1991@gmail.com>
2017-12-14 14:57:19 +0700

    fd

--- local/app/Http/Controllers/Backend/HighlightController.php
+++ local/app/Http/Controllers/Backend/HighlightController.php
@@ -47,7 +47,7 @@ class HighlightController extends Controller
 
       $filesystem = new Filesystem($adapter);
 
-      dd($client->rpcEndpointRequest('files/search', ['path' => '', 'query' => 'rakball']));
+      dd($client->rpcEndpointRequest('files/search', ['query' => 'rakball']));
       // $zones = Zone::orderBy('length')->get();
       //
       // $aStyle = array(
