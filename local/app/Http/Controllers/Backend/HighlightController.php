bbc45f67547b5ddd90d8756afc58b0eca05ed19d
Jaruwanno <jaruwanno1991@gmail.com>
2017-12-14 11:50:35 +0700

    fd

--- local/app/Http/Controllers/Backend/HighlightController.php
+++ local/app/Http/Controllers/Backend/HighlightController.php
@@ -47,7 +47,7 @@ class HighlightController extends Controller
 
       $filesystem = new Filesystem($adapter);
 
-      dd($client->listSharedLinks('/rakball24.png')); 
+      dd($client->read('/rakball24.png')); 
       // $zones = Zone::orderBy('length')->get();
       //
       // $aStyle = array(
