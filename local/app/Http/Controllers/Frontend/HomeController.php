85456b7ff42b9562c6a1fcb8a9ff7fc0dbf9d091
Jaruwanno <jaruwanno1991@gmail.com>
2017-12-23 21:47:47 +0700

    fd

--- local/app/Http/Controllers/Frontend/HomeController.php
+++ local/app/Http/Controllers/Frontend/HomeController.php
@@ -45,7 +45,7 @@ class HomeController extends Controller
       $result = curl_exec( $curl );
 
       $result = (array) json_decode($result);
-      dd($news);
+      dd($result);
       return view('frontend.home.home', [
         'news' => $news,
         'clip' => $clip,
