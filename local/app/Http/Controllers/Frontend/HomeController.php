c40fc3a00646e13d1639879c53a38053b82dba3d
Jaruwanno <jaruwanno1991@gmail.com>
2017-12-31 18:45:58 +0700

    fd

--- local/app/Http/Controllers/Frontend/HomeController.php
+++ local/app/Http/Controllers/Frontend/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
       );
 
       $aStyle = array(
-        'css/frontend/home/css.css'
+        'css/frontend/home/home_style.css'
       );
 
       $news = Post::where('type', 'news')->orderBy('created_at', 'desc')
