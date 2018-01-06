ad164b18950e9746bbf60c70c59703fcdcbb052d
Jaruwanno <jaruwanno1991@gmail.com>
2018-01-06 23:40:11 +0700

    fd

--- local/resources/views/layouts/frontend.blade.php
+++ local/resources/views/layouts/frontend.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-<head> 
+<head>
   <meta charset="utf-8">
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -13,7 +13,7 @@
   <!-- Styles -->
   <link rel="stylesheet" href="{{ asset( 'css/bootstrap/bootstrap.min.css' ) }}">
   <link rel="stylesheet" href="{{ asset( 'css/font awesome/css/font-awesome.min.css' ) }}">
-  <link rel="stylesheet" href="{{ asset( 'css/frontend/main.css' ) }}">
+  <link rel="stylesheet" href="{{ asset( 'css/frontend/main_style.css' ) }}">
 
   @if ( isset( $css ) )
     @foreach ($css as $style)
