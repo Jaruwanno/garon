8b2811dfb3d9fc2f38728ddb39fc5e3cb2409530
Jaruwanno <jaruwanno1991@gmail.com>
2018-01-07 18:33:20 +0700

    fd

--- local/resources/views/layouts/frontend.blade.php
+++ local/resources/views/layouts/frontend.blade.php
@@ -13,7 +13,7 @@
   <!-- Styles -->
   <link rel="stylesheet" href="{{ asset( 'css/bootstrap/bootstrap.min.css' ) }}">
   <link rel="stylesheet" href="{{ asset( 'css/font awesome/css/font-awesome.min.css' ) }}">
-  <link rel="stylesheet" href="{{ asset( 'css/frontend/main_style.css' ) }}">
+  <link rel="stylesheet" href="{{ asset( 'css/frontend/main.css' ) }}">
 
   @if ( isset( $css ) )
     @foreach ($css as $style)
