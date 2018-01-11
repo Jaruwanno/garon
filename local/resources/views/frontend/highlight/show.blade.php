f683fb6231046718f63fb32b627bbfc2e341070a
Jaruwanno <jaruwanno1991@gmail.com>
2018-01-11 17:50:23 +0700

    fd

--- local/resources/views/frontend/highlight/show.blade.php
+++ local/resources/views/frontend/highlight/show.blade.php
@@ -45,7 +45,7 @@
         </span>
       </span>
       <div class="clearfix"></div>
-      <video src="{{ $highlight[0]->link }}https://www.dropbox.com/s/d3o2tc76g3ybwp0/%E0%B9%80%E0%B8%8A%E0%B8%A5%E0%B8%8B%E0%B8%B5%20vs%20%E0%B8%AD%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B9%80%E0%B8%8B%E0%B8%99%E0%B8%AD%E0%B8%A5.mp4?raw=1"
+      <video src="https://www.dropbox.com/s/d3o2tc76g3ybwp0/%E0%B9%80%E0%B8%8A%E0%B8%A5%E0%B8%8B%E0%B8%B5%20vs%20%E0%B8%AD%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B9%80%E0%B8%8B%E0%B8%99%E0%B8%AD%E0%B8%A5.mp4?raw=1"
          type='video/mp4'
          controls
          width="100%"
