79865bf87af903126357f8cd3a8e8ac4534b288c
Jaruwanno <jaruwanno1991@gmail.com>
2018-01-11 14:42:08 +0700

    fd

--- local/resources/views/frontend/highlight/show.blade.php
+++ local/resources/views/frontend/highlight/show.blade.php
@@ -45,13 +45,16 @@
         </span>
       </span>
       <div class="clearfix"></div>
-      <video class="video-js vjs-16-9 vjs-big-play-centered" preload="auto"
+      <video width="320" height="240" poster="{{ Storage::disk('cover')->has($highlight[0]->path_cover) ? route('image', ['filename' => $highlight[0]->path_cover]) : '' }}" controls>
+         <source src="{{ $highlight[0]->link }}" type="video/mp4">
+      </video>
+      {{-- <video class="video-js vjs-16-9 vjs-big-play-centered" preload="auto"
           data-setup='{
               "poster": "{{ Storage::disk('cover')->has($highlight[0]->path_cover) ? route('image', ['filename' => $highlight[0]->path_cover]) : '' }}",
               "controls":true
           }'>
         <source src="{{ $highlight[0]->link }}" type="video/mp4">
-      </video>
+      </video> --}}
       <br><br>
       <div class="fb-comments" data-href="http://www.rakball24.com/news/{{$highlight[0]->id}}" data-width="100%" data-numposts="5"></div>
     </div>
