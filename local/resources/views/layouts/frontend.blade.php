68fec7f32064e2e353072c4d9225450ba79e7e7a
Jaruwanno <jaruwanno1991@gmail.com>
2018-01-12 20:32:51 +0700

    fd

--- local/resources/views/layouts/frontend.blade.php
+++ local/resources/views/layouts/frontend.blade.php
@@ -10,7 +10,7 @@
 
   <meta property="og:url"         content="" />
   <meta property="og:type"        content="website" />
-  <meta property="og:title"       content="เห้ยไอสัส" />
+  <meta property="og:title"       content="" />
   <meta property="og:description" content="Your description" />
   <meta property="og:image"       content="{{ asset('pic/file_error.png') }}" />
 
