2dfe83365dc22e8f2e0a8ab977c5ede60d5c79b5
Jaruwanno <jaruwanno1991@gmail.com>
2018-01-12 18:27:07 +0700

    fd

--- local/resources/views/layouts/frontend.blade.php
+++ local/resources/views/layouts/frontend.blade.php
@@ -2,11 +2,11 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
   <meta charset="utf-8">
-  <!-- CSRF Token --> 
+  <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">
+  {{-- <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">
   <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"> --}}
 
   <title>เอาใจคนรักบอล</title>
 
