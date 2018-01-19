793e10ba9d361c6aaca22f48abb193aa565ebbf2
Jaruwanno <jaruwanno1991@gmail.com>
2018-01-19 18:28:45 +0700

    fd

--- local/app/User.php
+++ local/app/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password'
+        'name', 'email'
     ];
 
     /**
@@ -23,7 +23,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'remember_token',
+        'password', 'remember_token',
     ];
 
 
