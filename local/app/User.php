adafa26acb0cf927b07b30cc0648493b28572139
Jaruwanno <jaruwanno1991@gmail.com>
2018-01-19 14:43:17 +0700

    fd

--- local/app/User.php
+++ local/app/User.php
@@ -8,6 +8,24 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'role_name'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
 
     public function hasAnyRole($roles){
       if( is_array( $roles ) ){
@@ -38,22 +56,7 @@ class User extends Authenticatable
     }
 
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password', 'role_name'
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
+    public function userProviders(){
+      return $this->hasMany('App\UserProviders');
+    }
 }
--- local/app/UserProviders.php
+++ local/app/UserProviders.php
@@ -4,7 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class UserProviders extends Model
+class UserProviders extends Model 
 {
-    //
+    protected $fillable = [
+      'user_id', 'avatar_url', 'provider_id', 'provider'
+    ];
 }
